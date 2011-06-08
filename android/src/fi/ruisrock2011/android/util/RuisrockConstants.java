643c7df9754747fb03bfd827dafc929771e7c92f
plah <plah@futurice.com>
2011-06-08 06:59:50 +0000

    Android dev.
    git-svn-id: https://svn.futurice.com/svn/fonecta_ruisrock@98 a7540e4b-867f-4f14-bb96-4de0f0f6a220

--- android/src/fi/ruisrock2011/android/util/RuisrockConstants.java
+++ android/src/fi/ruisrock2011/android/util/RuisrockConstants.java
@@ -24,20 +24,19 @@ public class RuisrockConstants {
 	public static final String PREFERENCE_GLOBAL = "fi.ruisrock2011.android.globalPreference";
 	
 	// INITIAL ETAG VALUES
-	/*
 	public static final String ETAG_GIGS = "5f1b28578706b53a4850ec76c1d77332";
 	public static final String ETAG_TRANSPORTATION = "65afc40ef57359e62246b118407d695a";
 	public static final String ETAG_NEWS = "ab4f962aeb683cb81c9006aa7ab71eb6";
 	public static final String ETAG_FOOD_AND_DRINK = "077fadec446efd20f8d15337989cc410";
 	public static final String ETAG_SERVICES = "369d5f3abd8c9106f2f17e9bc4741e67";
 	public static final String ETAG_GENERAL_INFO = "38b9ba53c1b6edb0c5835cd31084f247";
-	*/
 	
+	/*
 	public static final String ETAG_GIGS = null;
 	public static final String ETAG_TRANSPORTATION = null;
 	public static final String ETAG_NEWS = null;
 	public static final String ETAG_FOOD_AND_DRINK = null;
 	public static final String ETAG_SERVICES = null;
 	public static final String ETAG_GENERAL_INFO = null;
-	
+	*/
 }
