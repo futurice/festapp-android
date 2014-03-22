5901277788c4abfc542ef492d18843eb836d9264
Heidi Lehtevä <heidi.lehteva@futurice.com>
2014-03-22 16:38:49 +0200

    Find and remove hardcoded strings

--- android/src/fi/ruisrock/android/RuisrockMainActivity.java
+++ android/src/fi/ruisrock/android/RuisrockMainActivity.java
@@ -116,7 +116,7 @@ public class RuisrockMainActivity extends BaseActivity {
 	private void showFAQ() {
 		Intent intent = new Intent(this, InfoSubPageActivity.class);
 		intent.putExtra("subPageContent", ConfigDAO.getAttributeValue(ConfigDAO.ATTR_PAGE_GENERALINFO_FREQUENTLY_ASKED, getBaseContext()));
-		intent.putExtra("subPageTitle", getString(R.string.faq));
+		intent.putExtra("subPageTitle", getString(R.string.frequently_asked_questions));
 		startActivity(intent);
 	}
 }
