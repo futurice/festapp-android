8aa89ebf4a69c5a64da1d6a0de459d8d552bd90c
sorja <juhakunnas@dhcp-87-157.eduroam.aalto.fi>
2014-04-11 20:10:01 +0300

    Updated files added weekdays

--- src/com/futurice/festapp/domain/to/FestivalDay.java
+++ src/com/futurice/festapp/domain/to/FestivalDay.java
@@ -1,18 +1,30 @@
 package com.futurice.festapp.domain.to;
 
 public enum FestivalDay {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
 	FRIDAY,
 	SATURDAY,
 	SUNDAY;
 	
 	public String getFinnishName() {
 		switch (this) {
-		case FRIDAY:
-			return "PERJANTAI";
+         case MONDAY:
+            return R.string.Monday;
+         case TUESDAY:
+            return R.string.Tuesday;
+		case WEDNESDAY:
+            return R.string.Wednesday;
+        case THURSDAY:
+            return R.string.Thursday;
+        case FRIDAY:
+			return R.string.Friday;
 		case SATURDAY:
-			return "LAUANTAI";
+			return R.string.Thursday;
 		case SUNDAY:
-			return "SUNNUNTAI";
+			return R.string.Sunday;
 		default:
 			return null;
 		}
