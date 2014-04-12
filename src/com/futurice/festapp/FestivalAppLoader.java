9c6443f67f00dbd73346d8bceb3d15b0170ebf09
João Costa <joao.miguel.costa@gmail.com>
2014-04-12 14:57:45 +0300

    Refractored class that calls the loading method

--- src/com/futurice/festapp/FestivalAppLoader.java
+++ src/com/futurice/festapp/FestivalAppLoader.java
@@ -5,11 +5,13 @@ import com.futurice.festapp.util.CalendarUtil;
 import android.app.Application;
 import android.content.Context;
 
-public class CalendarWeekdayLoader extends Application{
+public class FestivalAppLoader extends Application{
 
     @Override
     public void onCreate() {
         super.onCreate();
+        
+        //Get weekdays names from resources
         CalendarUtil.load(this);;
     }
 
