d42620148bbb1fa4d6c78686395b17f18d029484
Matias Hilden <domewolf@gmail.com>
2014-04-12 06:37:47 +0300

    Removed unused import

--- src/com/futurice/festapp/service/ServiceBroadcastReceiver.java
+++ src/com/futurice/festapp/service/ServiceBroadcastReceiver.java
@@ -3,7 +3,6 @@ package com.futurice.festapp.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class ServiceBroadcastReceiver extends BroadcastReceiver {
 	
