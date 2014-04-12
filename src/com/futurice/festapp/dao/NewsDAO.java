156b819c77c05ab3f83807e398500ebe5a4e5b79
WaDelma <delma@MahComputer>
2014-04-12 12:10:33 +0300

    Refactored hard coded indexes to constants

--- src/com/futurice/festapp/dao/NewsDAO.java
+++ src/com/futurice/festapp/dao/NewsDAO.java
@@ -32,6 +32,10 @@ public class NewsDAO {
 	private static final DateFormat DB_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 	private static final String TAG = "NewsDAO";
 	private static final String[] NEWS_COLUMNS = { "url", "title", "newsDate", "content" };
+	private static final int URL = 0;
+	private static final int TITLE = 1;
+	private static final int NEWS_DATE = 2;
+	private static final int CONTENT = 3;
 	
 	public static List<NewsArticle> findAll(Context context) {
 		List<NewsArticle> articles = new ArrayList<NewsArticle>();
@@ -41,10 +45,10 @@ public class NewsDAO {
 			db = (new DatabaseHelper(context)).getWritableDatabase();
 			cursor = db.rawQuery("SELECT url, title, newsDate, content FROM news ORDER BY newsDate DESC", new String[]{});
 			while (cursor.moveToNext()) {
-				String url = cursor.getString(0);
-		        String title = cursor.getString(1);
-		        Date date = getDate(cursor.getString(2));
-		        String content = cursor.getString(3);
+				String url = cursor.getString(URL);
+		        String title = cursor.getString(TITLE);
+		        Date date = getDate(cursor.getString(NEWS_DATE));
+		        String content = cursor.getString(CONTENT);
 		        articles.add(new NewsArticle(url, title, date, content));
 			}
 		} finally {
@@ -142,10 +146,10 @@ public class NewsDAO {
 	
 	private static NewsArticle convertCursorToNewsArticle(Cursor cursor) {
 		return new NewsArticle(
-				cursor.getString(0),
-				cursor.getString(1),
-				getDate(cursor.getString(2)),
-				cursor.getString(3));
+				cursor.getString(URL),
+				cursor.getString(TITLE),
+				getDate(cursor.getString(NEWS_DATE)),
+				cursor.getString(CONTENT));
 	}
 	
 	public static List<NewsArticle> parseFromJson(String json) throws Exception {
