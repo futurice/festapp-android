c5937ce39a4d1f206bfa4690ecf6decfd34d57d7
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2014-04-13 11:24:31 +0300

    partial fixes

--- src/com/futurice/festapp/domain/to/FestivalDay.java
+++ src/com/futurice/festapp/domain/to/FestivalDay.java
@@ -1,29 +1,22 @@
 package com.futurice.festapp.domain.to;
 
-
 import com.futurice.festapp.R;
 import android.content.Context;
 
 public enum FestivalDay {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-	FRIDAY,
-	SATURDAY,
-	SUNDAY;
-	
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
 	public String getLocalName(Context context) {
 		switch (this) {
-        case MONDAY:
-            return context.getString(R.string.Monday);
-        case TUESDAY:
-            return context.getString(R.string.Tuesday);
+		case MONDAY:
+			return context.getString(R.string.Monday);
+		case TUESDAY:
+			return context.getString(R.string.Tuesday);
 		case WEDNESDAY:
-            return context.getString(R.string.Wednesday);
-        case THURSDAY:
-            return context.getString(R.string.Thursday);
-        case FRIDAY:
+			return context.getString(R.string.Wednesday);
+		case THURSDAY:
+			return context.getString(R.string.Thursday);
+		case FRIDAY:
 			return context.getString(R.string.Friday);
 		case SATURDAY:
 			return context.getString(R.string.Saturday);
@@ -33,5 +26,25 @@ public enum FestivalDay {
 			return null;
 		}
 	}
-	
+
+	public String getLocalAbbrv(Context context) {
+		switch (this) {
+		case MONDAY:
+			return context.getString(R.string.Mon);
+		case TUESDAY:
+			return context.getString(R.string.Tue);
+		case WEDNESDAY:
+			return context.getString(R.string.Wed);
+		case THURSDAY:
+			return context.getString(R.string.Thu);
+		case FRIDAY:
+			return context.getString(R.string.Fri);
+		case SATURDAY:
+			return context.getString(R.string.Sat);
+		case SUNDAY:
+			return context.getString(R.string.Sun);
+		default:
+			return null;
+		}
+	}
 }
